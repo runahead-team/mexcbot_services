4be8f488fe2620ecf8b3b53fd353622bc5096736
Nguyen Nam <>
2022-10-26 00:42:58 +0700

    update

--- src/multexbot.Api/Infrastructure/ExchangeClient/BingxClient.cs
+++ src/multexbot.Api/Infrastructure/ExchangeClient/BingxClient.cs
@@ -39,64 +39,72 @@ namespace multexBot.Api.Infrastructure.ExchangeClient
         public override async Task<(decimal LastPrice, decimal LastPriceUsd, decimal OpenPrice)> GetMarket(string @base,
             string quote)
         {
-            try
-            {
-                var (success, responseBody) =
-                    await SendRequest("GET", $"api/v1/market/getTicker?symbol={@base}-{quote}");
+            var (success, responseBody) =
+                await SendRequest("GET",
+                    $"https://api-swap-rest.bingbon.pro/api/v1/market/getTicker?symbol={@base}-{quote}");
 
-                if (!success)
-                    return (0, 0, 0);
-
-                var ticker = JObject.Parse(responseBody)["data"]?["tickers"]?[0];
-
-                return (
-                    decimal.Parse(ticker["tradePrice"].ToString()),
-                    decimal.Parse(ticker["tradePrice"].ToString()),
-                    decimal.Parse(ticker["openPrice"].ToString())
-                );
-            }
-            catch (Exception e)
-            {
+            if (!success)
                 return (0, 0, 0);
-            }
+
+            var ticker = JObject.Parse(responseBody)["data"]?["tickers"]?[0];
+
+            return (
+                decimal.Parse(ticker["tradePrice"].ToString()),
+                decimal.Parse(ticker["tradePrice"].ToString()),
+                decimal.Parse(ticker["openPrice"].ToString())
+            );
         }
 
         public override async Task<OrderDto> CreateLimitOrder(string @base, string quote, decimal amount, decimal price,
             OrderSide side)
         {
-            try
-            {
-                var payload = $"symbol={@base}-{quote}&side={side:G}&type=LIMIT&price={price}&quantity={amount}";
+            var payload = $"symbol={@base}-{quote}&side={side:G}&type=LIMIT&price={price}&quantity={amount}";
 
-                var (success, responseBody) =
-                    await SendRequest("POST", $"/openApi/spot/v1/trade/order?{payload}");
+            var (success, responseBody) =
+                await SendRequest("POST", $"/openApi/spot/v1/trade/order?{payload}");
 
-                if (!success)
-                    return null;
-
-                var order = JObject.Parse(responseBody)["data"];
-
-                var orderDto = new OrderDto
-                {
-                    Base = @base,
-                    Quote = quote,
-                    Qty = amount,
-                    Price = price,
-                    Side = side,
-                    ExternalId = long.Parse(order["orderId"].ToString())
-                };
-
-                return orderDto;
-            }
-            catch (Exception)
-            {
+            if (!success)
                 return null;
-            }
+
+            var order = JObject.Parse(responseBody)["data"];
+
+            var orderDto = new OrderDto
+            {
+                Base = @base,
+                Quote = quote,
+                Qty = amount,
+                Price = price,
+                Side = side,
+                ExternalId = long.Parse(order["orderId"].ToString())
+            };
+
+            return orderDto;
         }
 
-        public override Task<List<OrderDto>> GetOpenOrders(string @base, string quote)
+        public override async Task<List<OrderDto>> GetOpenOrders(string @base, string quote)
         {
-            throw new NotImplementedException();
+            var (success, responseBody) =
+                await SendRequest("GET", $" /openApi/spot/v1/trade/openOrders?symbol={@base}-{quote}");
+
+            if (!success)
+                return new List<OrderDto>();
+
+            var orders = JObject.Parse(responseBody)["data"]?["orders"];
+
+            var returnOrders = new List<OrderDto>();
+
+            foreach (var order in orders)
+            {
+                returnOrders.Add(new OrderDto
+                {
+                    ExternalId = (long) order["orderId"],
+                    Price = decimal.Parse(order["price"].ToString()),
+                    Qty = decimal.Parse(order["origQty"].ToString()),
+                    Filled = decimal.Parse(order["executedQty"].ToString()),
+                });
+            }
+
+            return returnOrders;
         }
 
         public override async Task<bool> Cancel(string id, string @base = null, string quote = null)
@@ -151,7 +159,10 @@ namespace multexBot.Api.Infrastructure.ExchangeClient
         private async Task<(bool, string)> SendRequest(string method, string endpoint, string payload = "",
             bool useSignature = false, bool logInfo = true)
         {
-            var uri = new Uri(_baseUri, endpoint);
+            var uri =
+                endpoint.StartsWith("https")
+                    ? new Uri(endpoint)
+                    : new Uri(_baseUri, endpoint);
 
             if (logInfo)
                 Log.Information($"BingxClient:SendRequest request {endpoint} {payload}");
