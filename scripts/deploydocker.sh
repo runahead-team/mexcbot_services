48493943b8a3fe691df9e6a7f287e36459678b89
Nam Nguyen <namnguyen@Nams-MacBook-Pro.local>
2023-05-12 18:36:42 +0700

    update docker

--- scripts/deploydocker.sh
+++ scripts/deploydocker.sh
@@ -3,6 +3,6 @@ tag=$1;
 
 echo "build mexcbot_services:$tag"
 
-docker build -t mexcbot_services:$tag -f ../src/mexcbot.Api/Dockerfile ../src
+docker buildx build --platform linux/amd64 -t mexcbot_services:$tag -f ../src/mexcbot.Api/Dockerfile ../src
 docker tag mexcbot_services:$tag ra25/mexcbot_services:$tag
 docker push ra25/mexcbot_services:$tag
