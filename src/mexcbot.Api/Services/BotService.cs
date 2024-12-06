c912eb6802be50f5081397e4c2fdecd511a525a9
chris <nnam2404@gmail.com>
2024-12-06 12:48:15 +0700

    update

--- src/mexcbot.Api/Services/BotService.cs
+++ src/mexcbot.Api/Services/BotService.cs
@@ -151,31 +151,31 @@ namespace mexcbot.Api.Services
             bot.Base = bot.ExchangeType == BotExchangeType.LBANK ? bot.Base.ToLower() : bot.Base;
             bot.Quote = bot.ExchangeType == BotExchangeType.LBANK ? bot.Quote.ToLower() : bot.Quote;
 
-            ExchangeClient client = bot.ExchangeType switch
-            {
-                BotExchangeType.MEXC => new MexcClient(Configurations.MexcUrl, bot.ApiKey, bot.ApiSecret),
-                BotExchangeType.LBANK => new LBankClient(Configurations.LBankUrl, bot.ApiKey, bot.ApiSecret),
-                BotExchangeType.DEEPCOIN => new DeepCoinClient(Configurations.DeepCoinUrl, bot.ApiKey, bot.ApiSecret, bot.Passphrase),
-                _ => null
-            };
+            // ExchangeClient client = bot.ExchangeType switch
+            // {
+            //     BotExchangeType.MEXC => new MexcClient(Configurations.MexcUrl, bot.ApiKey, bot.ApiSecret),
+            //     BotExchangeType.LBANK => new LBankClient(Configurations.LBankUrl, bot.ApiKey, bot.ApiSecret),
+            //     BotExchangeType.DEEPCOIN => new DeepCoinClient(Configurations.DeepCoinUrl, bot.ApiKey, bot.ApiSecret, bot.Passphrase),
+            //     _ => null
+            // };
 
-            if (client != null)
-            {
-                var exchangeInfo = await client.GetExchangeInfo(bot.Base, bot.Quote);
-                var accBalances = await client.GetAccInformation();
-
-                var accInfo = new AccInfo
-                {
-                    Balances = accBalances
-                };
-
-                bot.ExchangeInfo = (exchangeInfo == null || string.IsNullOrEmpty(exchangeInfo.Symbol))
-                    ? string.Empty
-                    : JsonConvert.SerializeObject(exchangeInfo);
-                bot.AccountInfo = (!accBalances.Any())
-                    ? string.Empty
-                    : JsonConvert.SerializeObject(accInfo);
-            }
+            // if (client != null)
+            // {
+            //     var exchangeInfo = await client.GetExchangeInfo(bot.Base, bot.Quote);
+            //     var accBalances = await client.GetAccInformation();
+            //
+            //     var accInfo = new AccInfo
+            //     {
+            //         Balances = accBalances
+            //     };
+            //
+            //     bot.ExchangeInfo = (exchangeInfo == null || string.IsNullOrEmpty(exchangeInfo.Symbol))
+            //         ? string.Empty
+            //         : JsonConvert.SerializeObject(exchangeInfo);
+            //     bot.AccountInfo = (!accBalances.Any())
+            //         ? string.Empty
+            //         : JsonConvert.SerializeObject(accInfo);
+            // }
             
             await DbConnections.ExecAsync(async (dbConnection) =>
             {
@@ -185,7 +185,7 @@ namespace mexcbot.Api.Services
                     throw new AppException("Bot is not exist");
 
                 var exec = await dbConnection.ExecuteAsync(
-                    @"UPDATE Bots SET `Base` = @Base, `Quote` = @Quote, VolumeOption = @VolumeOption, MakerOption = @MakerOption, NextRunMakerTime = 0
+                    @"UPDATE Bots SET `Base` = @Base, `Quote` = @Quote, Side = @Side, VolumeOption = @VolumeOption, MakerOption = @MakerOption, NextRunMakerTime = 0
                     WHERE UserId = @UserId AND Id = @Id AND Type = @Type",
                     bot);
 
